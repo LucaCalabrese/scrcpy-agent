11ff0cf3925aa68781e133e756c78edd2fa46c75
luca.calabrese <luca.calabrese@kaleyra.com>
2022-04-04 16:41:24 +0200

    Initial Commit

---
+++ scrcpy-agent/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  scrcpy-agent
+//
+//  Created by luca.calabrese on 04/04/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ scrcpy-agentUITests/scrcpy_agentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  scrcpy_agentUITestsLaunchTests.swift
+//  scrcpy-agentUITests
+//
+//  Created by luca.calabrese on 04/04/22.
+//
+
+import XCTest
+
+class scrcpy_agentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
